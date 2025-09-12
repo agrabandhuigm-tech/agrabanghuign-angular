3942d2e37feced47984d0a80dc01c0966d9ed2a0
Bharat <agrabandhuigm@gmail.com>
2025-09-12 18:48:40 +0530

    a

--- src/app/main/mydashboard/mydashboard.component.ts
+++ src/app/main/mydashboard/mydashboard.component.ts
@@ -1,82 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
-
 
 @Component({
   selector: 'app-mydashboard',
   templateUrl: './mydashboard.component.html',
   styleUrls: ['./mydashboard.component.scss']
 })
-
-export class MydashboardComponent implements OnInit, AfterViewInit, OnChanges {
-
-
-
-  constructor() {
-
-
-
-
-  }
-
-  ngOnInit(): void {
-
-
-
-  }
-  ngOnChanges(changes: SimpleChanges): void { }
-
-  ngAfterViewInit(): void {
-
-  }
+export class MydashboardComponent {
   slides = [
-    { img: 'assets/images/slide1.jpg', title: 'Welcome to Agarwal Samaj' },
-    { img: 'assets/images/slide2.jpg', title: 'Community Events' },
-    { img: 'assets/images/slide3.jpg', title: 'Our Leaders' }
+    { img: './assets/images/slide1.jpg', title: 'Welcome to Agarwal Samaj' },
+    { img: './assets/images/slide2.jpg', title: 'Community Events' }
   ];
-  // customOptions: OwlOptions = {
-  //   loop: true,
-  //   margin: 10,
-  //   mouseDrag: true,
-  //   touchDrag: true,
-  //   pullDrag: false,
-  //   dots: false,
-  //   navSpeed: 700,
-  //   navText: ['', ''],
-  //   responsive: {
-  //     0: {
-  //       items: 1,
-  //     },
-  //     320: {
-  //       items: 1.5,
-  //     },
-  //     400: {
-  //       items: 2.7,
-  //     },
-  //     500: {
-  //       items: 3.2,
-  //     },
-  //     740: {
-  //       items: 3,
-  //     },
-  //     880: {
-  //       items: 3.2,
-  //     },
 
-  //     940: {
-  //       items: 4,
-  //     },
-  //   },
-  //   nav: true,
-  // };
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
-    autoplayTimeout: 3000,
+    autoplayTimeout: 1000,
     autoplayHoverPause: true,
     dots: true,
-    nav: true,
-    navText: ['‹', '›'],
+     animateIn: 'fadeIn',  
+  animateOut: 'fadeOut',  
     responsive: {
       0: { items: 1 },
       600: { items: 1 },
@@ -84,8 +27,3 @@ export class MydashboardComponent implements OnInit, AfterViewInit, OnChanges {
     }
   };
 }
-
-
-
-
-
